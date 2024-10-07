83a711fb739b9e7b29b2cc42da0e54132c9c5402
Saifullah-max <saifullahahmed380@gmail.com>
2024-10-07 20:42:50 +0500

    added authorization for users and admin

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,8 @@ async function handleUserLogin(req, res) {
         error: 'Invalid email or password',
     })
 
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie('uid', sessionId);
+    const token = setUser(user);
+    res.cookie('token', token);
     return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,26 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Snaxx123@-$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if(!token) return null;
+  try {
+    return jwt.verify(token, secret);
+
+  } catch (error) {
+    console.error("Token verification failed:", error.message);
+    return null;
+  }
 }
 
 module.exports = {
