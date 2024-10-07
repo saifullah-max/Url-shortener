d008bfbf34613d573376f7db02b194086b2910e9
Saifullah-max <saifullahahmed380@gmail.com>
2024-10-07 20:42:50 +0500

    added authorization for users and admin

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,29 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next){
-    const userUid = req.cookies?.uid;
+function checkForAuthentication(req, res, next){
+    const tokenCookie = req.cookies?.token;
+    req.user = null;
+    if(!tokenCookie)
+        return next();
 
-    if(!userUid) return res.redirect("/login");
-    const user = getUser(userUid);
-
-    if(!user) return res.redirect("/login");
+    const token = tokenCookie;
+    const user = getUser(token);
 
     req.user = user;
     next();
 }
 
-async function checkAuth(req, res, next) {
-    const userUid = req.cookies?.uid;
+function restrictTo(roles= []){
+     return function (req, res, next) {
+        if (!req.user) return res.redirect("/login");
 
-    const user = getUser(userUid);
-
-    req.user = user;
-    next();
+        if(!roles.includes(req.user.role)) return res.end("UnAuthorized");
+        
+        return next();
+     }
 }
 
 module.exports = {
-    restrictToLoggedinUserOnly,
-    checkAuth,
+    checkForAuthentication,
+    restrictTo,
 }
